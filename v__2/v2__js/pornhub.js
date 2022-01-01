3c8e7ed9171980010a5214b7b64f8943390ac76b
webDotG <webdotg@gmail.com>
2022-01-01 19:18:19 +0300

    add alert popup onlyfans js add

--- v__2/v2__js/pornhub.js
+++ v__2/v2__js/pornhub.js
@@ -1,6 +1,9 @@
 const pornhub=document.querySelector(".pornhub__link");
 const pornhubShow=document.querySelector(".pornhub__alert--none");
 const pornhubClose=document.querySelector(".pornhub__button--no")
+const onlyfans=document.querySelector(".onlyfans__link");
+const onlyfansShow=document.querySelector(".onlyfans__alert--none");
+const onlyfansClose=document.querySelector(".onlyfans__button--no")
 
 pornhub.addEventListener("click", function (evt){
 	evt.preventDefault();
@@ -10,4 +13,13 @@ pornhub.addEventListener("click", function (evt){
 pornhubClose.addEventListener("click", function (evt){
 	evt.preventDefault();
   pornhubShow.classList.remove("pornhub__alert--show");
+})
+onlyfans.addEventListener("click", function (evt){
+	evt.preventDefault();
+	onlyfansShow.classList.add("onlyfans__alert--show");
+});
+
+onlyfansClose.addEventListener("click", function (evt){
+	evt.preventDefault();
+  onlyfansShow.classList.remove("onlyfans__alert--show");
 })
