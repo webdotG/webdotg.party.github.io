c163770ac4b605b994cb7df5bb1a6f249a0ab7ed
webDotG <webdotg@gmail.com>
2022-01-03 20:26:45 +0300

    test popup js

--- v__2/v2__js/about-page.js
+++ v__2/v2__js/about-page.js
@@ -4,11 +4,17 @@ const popup=document.querySelector(".modal-login");
 const form=popup.querySelector("form");
 const login=popup.querySelector("[name=login]");
 const password=popup.querySelector("[name=password]");
+const storage=localStorage.getItem("login");
+
+
 
 link.addEventListener("click", function(evt){
 	evt.preventDefault();
 	modal.classList.add("modal-show");
 	login.focus();
+	if(storage){
+		login.value=storage; 
+	}
 })
 
 form.addEventListener("submit", function(evt){
