9c0f5e8c08dd7bc8de4baaab0dc1a96cea626f3a
webDotG <webdotg@gmail.com>
2022-01-05 21:35:38 +0300

    index page js add

---
+++ warning.js
@@ -0,0 +1,15 @@
+const firstVersion=document.querySelector(".resp");
+const warningBlock=document.querySelector(".warning");
+const linkGood=document.querySelector(".warning-list__link--good");
+const warningClose=document.querySelector(".warning__close");
+
+firstVersion.addEventListener("click", function(evt){
+	evt.preventDefault();
+	warningBlock.classList.add("warning--show");
+	linkGood.focus();
+})
+
+warningClose.addEventListener("click", function(evt){
+	evt.preventDefault();
+	warningBlock.classList.remove("warning--show");
+})
