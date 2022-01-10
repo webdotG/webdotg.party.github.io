898c2d997a8d06ca98e144f89f6e8556ea645dc0
webDotG <webdotg@gmail.com>
2022-01-10 10:41:08 +0300

    del dvice page code

--- v__2/v2__js/sandbox-page.js
+++ v__2/v2__js/sandbox-page.js
@@ -1,15 +1,14 @@
-const shareOrReadIdeas = document.querySelector(".idea-link__share")
+const shareOrReadIdeas = document.querySelector(".idea-link__share");
+const postExample = document.querySelector(".post-example__wrapper");
+const registrWrapper = document.querySelector(".form-registr__wrapper");
+
+
+shareOrReadIdeas.addEventListener("click", function(evt){
+	evt.preventDefault();
+	shareOrReadIdeas.classList.add("display--none");
+	postExample.classList.add("display--none");
+	registrWrapper.classList.remove("display--none");
+});
 
 
 
-const formRegistrWrapper = document.querySelector(".form__wrapper");
-
-
-
-
-
-//const formRegistrSubmit = document.querySelector(".form__submit");
-//formRegistrSubmit.addEventListener("click", function (evt){
-//	evt.preventDefault();
-//	formRegistrWrapper.classList.add("form__wrapper--none");
-//});
