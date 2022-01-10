4d3ca4a1525ff7c5bdf5b733e33f3166c43d47eb
webDotG <webdotg@gmail.com>
2022-01-11 02:16:32 +0300

    sandbox popup position top 10px

--- v__2/v2__js/sandbox-page.js
+++ v__2/v2__js/sandbox-page.js
@@ -1,5 +1,6 @@
 const shareOrReadIdeas = document.querySelector(".idea-link__share");
 const postExample = document.querySelector(".post-example");
+const exampleTile = document.querySelector(".post-example__title")
 const formRegistrSandbox = document.querySelector(".form-registr__wrapper");
 
 
@@ -7,6 +8,7 @@ shareOrReadIdeas.addEventListener("click", function(evt){
 	evt.preventDefault();
 	shareOrReadIdeas.classList.add("display--none");
 	postExample.classList.add("display--none");
+	exampleTile.classList.add("display--none");
 	formRegistrSandbox.classList.remove("display--none");
 });
 
