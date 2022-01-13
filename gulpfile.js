7eebf16ac866f970006d3c2564eda0b7d5407a29
webDotG <webdotg@gmail.com>
2022-01-13 16:27:09 +0300

    update gulpfile need registr paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,93 @@
-const c_sass = require('gulp-sass')(require('sass'));
-const c_less = require ('gulp-less');
-
-const preprocessor = {
-	name: 'sass',
-	executor: c_sass
-}
-
-const {src, dest, parallel, series, watch} = require ('gulp');
-const c_browserSync = require('browser-sync').create();
-const  c_concat = require('gulp-concat');
-const c_uglify = require('gulp-uglify-es').default;
-const c_autoprefixer = require('gulp-autoprefixer');
-const c_cleancss = require('gulp-clean-css');
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
 const del = require('del');
-
-
 const imagemin = require('gulp-imagemin');
-function images () {
-	gulp.src('./img/*')
-		.pipe(imagemin(
-			[
-				gifsicle({interlaced: true}),
-				mozjpeg({quality: 75, progressive: true}),
-				optipng({optimizationLevel: 5}),
-				svgo({
-					plugins: [
-						{
-							name: 'removeViewBox',
-							active: true
-						},
-						{
-							name: 'cleanupIDs',
-							active: false
-						}
-					]
-				})
-			]
-		))
-		.pipe(gulp.dest('./img-compress'))
-	};
 
-
-function browsersync() {
-    c_browserSync.init({
-        server:{ baseDir:'app/' },
-        notify:true,
-        online:false
-    })
-};
-
+function browsersync (){
+	browserSync.init({
+		server:{ baseDir: 'app/' },
+		notify: false,
+		online:false
+	})
+}
 function scripts(){
-    return src([
-        'node_modules/jquery/dist/jquery.min.js',
-        'app/js/app.js',
-    ])
-        .pipe(c_concat('app.min.js'))
-				.pipe(c_uglify())
-        .pipe(dest('app/js'))
-				.pipe(c_browserSync.stream())
+	return src([
+			'node_modules/jquery/dist/jquery.min.js',
+			'app/js/app.js',
+	])
+			.pipe(concat('app.min.js'))
+			.pipe(uglify()) 
+			.pipe(dest('app/js'))
+			.pipe(browserSync.stream())
 };
 
 function styles(){
-	return src('app/' + preprocessor.name + '/main.' + preprocessor.name + '')
-		.pipe(preprocessor.executor())
-		// return src('app/sass/main.sass')
- 		//.pipe(c_sass())
-		.pipe(c_concat('app.min.css'))
-		.pipe(c_autoprefixer({ overrideBrowserslist: ['last 10 version'], grid:true }))
- 		.pipe(c_cleancss(( { level:{ 1: { specialComments:0 } } , format:'beautify'} )))
-		.pipe(dest('app/css'))
-		.pipe(c_browserSync.stream())
-};
-
-function cleandist(){
-	return del('dist/**/*', {force:true})
-}
-
-function buildcopy(){
-	return src([
-		'app/css**/*.min.css',
-		'app/js/**/*.min.js',
-//  'app/images/dest/**/*', need fix compess-img
-		'app/**/*.html'
-	], { base:'app' })
-	.pipe(dest('dist'));
+	return src('app/sass/*.sass')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(concat('style.min.css'))
+	.pipe(autoprefixer({ overridBrowserslist:['last 10 versions'], grid:true }))
+	.pipe(cleancss(( { level:{1:{specialComments:0} }, /*format:'beautify'*/ } )))
+	.pipe(dest('app/css'))
+	.pipe(browserSync.stream())
 }
 
 function startwatch(){
-	watch('app/**/*.html').on('change', c_browserSync.reload)
-	watch('app/**/' + preprocessor + '**/*',styles)
-	watch('app/sass/*.sass',styles);
-	watch(['app/**/*.js', '!app/**/*min.js'], scripts);
+	watch('app/**/*.html').on('change', browserSync.reload);
+	watch('app/**/*.sass', styles);
+	watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+	//watch('app/images/**/*', images);
 }
+
+function images(){
+	return src('app/images/**/*')
+	.pipe(imagemin(
+		[
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.mozjpeg({quality: 75, progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: true},
+					{cleanupIDs: false}
+				]
+			})
+		]
+	))
+	.pipe(dest('dist/images'))
+}
+
+ function build() {
+	return src([  // выбор файлов
+		'app/**/*.html',
+		'app/fonts/**/*',
+		'app/css/**/*.min.css',
+		'app/js/**/*.min.js',
+		//'app/images/**/*',
+		], { base: 'app' }) //  сохраняю структуру проекта при копировании
+	.pipe(dest('dist')) // выгружаю в папку с финальной сборкой
+}
+
+//----------------------------------------------на случай чистки-----------------------------------
+function cleanimg() {
+	return del('app/images/src/**/*', { force: true }) //автономно если захочу удалить содержимое
+ }
+function cleandist() {
+	return del('dist/**/*', { force: true }) //автономно если захочу удалить содержимое dist
+}
+exports.cleandist = cleandist	//автономно если захочу удалить содержимое
+exports.cleanimg = cleanimg;	//автономно если захочу удалить содержимое
+
 
 exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.styles = styles;
-// exports.images = images;
+exports.images = images;
 
-exports.build = series(cleandist ,styles, scripts, /*images*/ buildcopy)
+exports.build = series(cleandist, styles, scripts, build);
 
-exports.default = parallel( styles, scripts, browsersync, startwatch);
+exports.default = parallel(styles, scripts, browsersync, startwatch);
